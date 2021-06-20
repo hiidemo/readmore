dab3aa610999f42ee5386fe411b411a4e4f7660d
Nguyen Quang Huy <ihomed@gmail.com>
2021-06-20 13:28:10 +0700

    init commit

---
+++ lang/en.js
@@ -0,0 +1,8 @@
+﻿/*
+Copyright (c) 2003-2013, Cricri042. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang( 'readmore', 'en', {
+	readmore_less_text	: 'Show less',
+	readmore_more_text : 'Read more'
+});
---
+++ lang/vi.js
@@ -0,0 +1,8 @@
+﻿/*
+Copyright (c) 2003-2013, Cricri042. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang( 'readmore', 'vi', {
+	readmore_less_text	: 'Thu gọn',
+	readmore_more_text : 'Đọc thêm'
+});
